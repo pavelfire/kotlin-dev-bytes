bb156ce2ca428b55746bcdd9fe93fbe415bc0853
Pavel MSI GL62 <fire_ra@bk.ru>
2021-04-11 13:20:22 +0300

    lesson 9-14 Build a repository

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,3 +16,35 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.Video
+import com.example.android.devbyteviewer.network.Network
+import com.example.android.devbyteviewer.network.asDatabaseModel
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+
+class VideosRepository(private val database: VideosDatabase){
+
+    //A playlist of videos that can be shown on the screen.
+    val vdeos: LiveData<List<Video>> = Transformations.map(database.videoDao.getVideos()){
+        it.asDomainModel()
+    }
+    //Repository for fetching devbyte videos from the network and storing them on disk.
+    //Refresh videos stored in the offline cache.
+    //This function uses the IO dispatcher to ensure the database insert database operation
+    //happens on the IO dispatcher. By switching to the IO dispatcher using 'withContext' this
+    //function is now safe to call from any thread including the Main thread.
+    //To actually load the videos for use, observe [videos]
+    suspend fun refreshVideos(){
+        withContext(Dispatchers.IO) {
+            val playlist = Network.devbytes.getPlaylist().await()
+            database.videoDao.insertAll(*playlist.asDatabaseModel())
+        }
+    }
+}
